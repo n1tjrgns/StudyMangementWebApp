e50cd261640a0506e07fe13e468d2e36367e0bd1
seokhun <n2tjrgns@gmail.com>
2020-07-11 17:33:41 +0900

    회원가입 Validator 작성

--- src/main/java/com/jpa/studywebapp/account/AccountController.java
+++ src/main/java/com/jpa/studywebapp/account/AccountController.java
@@ -1,21 +1,46 @@
 package com.jpa.studywebapp.account;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
 
+    private final SignUpFormValidator signUpFormValidator;
+
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder){
+        webDataBinder.addValidators(signUpFormValidator);
+    }
+
     @GetMapping("/sign-up")
-    public String signUpForm(HttpServletRequest request, Model model){
+    public String signUpForm(Model model){
 
         //속성명과 클래스 이름이 같다면 생략가능
        //model.addAttribute("signUpForm", new SignUpForm());
         model.addAttribute(new SignUpForm());
 
         return "account/sign-up";
+    }
+
+    //form에 대한 validation 검증
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors){
+        if(errors.hasErrors()){
+            return "account/sign-up";
+        }
+
+        //정상통과시 메인 페이지로
+       return "redirect:/";
     }
 }
---
+++ src/main/java/com/jpa/studywebapp/account/AccountRepository.java
@@ -0,0 +1,13 @@
+package com.jpa.studywebapp.account;
+
+import com.jpa.studywebapp.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+//select만 사용 할 곳에서는 readOnly를 줌으로써 조금이라도 성능 향상을 꾀할 수 있다.
+@Transactional(readOnly = true)
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    boolean existsByEmail(String email);
+
+    boolean existsByNickname(String nickname);
+}
