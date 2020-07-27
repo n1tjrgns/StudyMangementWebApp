1eedc87cee14440bca0fb28d01a5a41185d2a189
seokhun <n2tjrgns@gmail.com>
2020-07-27 22:37:42 +0900

    태그 엔티티 추가 ManyToMany (Account -> Tag) 단방향

--- src/main/java/com/jpa/studywebapp/domain/Account.java
+++ src/main/java/com/jpa/studywebapp/domain/Account.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -65,6 +66,9 @@ public class Account {
     private boolean studyUpdatedByEmail;
 
     private boolean studyUpdatedByWeb = true;
+
+    @ManyToMany
+    private Set<Tag> tags;
 
     public void generateEmailCheckToken() {
         this.emailCheckToken = UUID.randomUUID().toString();
