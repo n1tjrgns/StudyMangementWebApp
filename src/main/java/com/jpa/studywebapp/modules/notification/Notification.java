c230ee833ee698725052a5238d8e0ebc165fc492
seokhun <n2tjrgns@gmail.com>
2020-08-18 13:45:58 +0900

    알림 도메인 추가

---
+++ src/main/java/com/jpa/studywebapp/modules/notification/Notification.java
@@ -0,0 +1,35 @@
+package com.jpa.studywebapp.modules.notification;
+
+import com.jpa.studywebapp.modules.account.Account;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@NoArgsConstructor @EqualsAndHashCode(of = "id")
+public class Notification {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String title;
+
+    private String link;
+
+    private String message;
+
+    private boolean checked;
+
+    @ManyToOne
+    private Account account;
+
+    private LocalDateTime createdLocalDateTime;
+
+    @Enumerated(EnumType.STRING)
+    private NotificationType notificationType;
+}
