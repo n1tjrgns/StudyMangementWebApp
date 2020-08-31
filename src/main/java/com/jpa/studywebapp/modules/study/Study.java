a4d4b80dc55ddb10b5893858fba8afffc35bcdb4
seokhun <n2tjrgns@gmail.com>
2020-08-31 23:16:15 +0900

    Revert "스터디 default 이미지 추가, 로고 이미지 수정"
    This reverts commit 60075d6d

--- src/main/java/com/jpa/studywebapp/modules/study/Study.java
+++ src/main/java/com/jpa/studywebapp/modules/study/Study.java
@@ -190,8 +190,4 @@ public class Study {
     public String getEncodedPath() throws UnsupportedEncodingException {
         return URLEncoder.encode(this.path, String.valueOf(StandardCharsets.UTF_8));
     }
-
-    public String getImage() {
-        return image != null ? image : "/images/default_banner.jpg";
-    }
 }
